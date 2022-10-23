992e1453fe0b6f91429e38221fc64ab0c3d2af45
yenlio <yenliot4311@gmail.com>
2022-10-23 21:50:47 +0700

    code display

--- src/app/presentation/layout/home/home.component.ts
+++ src/app/presentation/layout/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AdminService } from 'src/app/data/service/admin.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  dataTour!:any
+  constructor(private AdminService: AdminService) { }
 
   ngOnInit(): void {
+    this.getPostTour()
   }
+  getPostTour(){
+    this.AdminService.getTour().subscribe((res)=>{
+    this.dataTour=res.data
+     console.log(res.data,"data");
+     
+      
+    })
+    }
+
 
 }
